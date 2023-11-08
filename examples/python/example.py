19c205a5cc8e165871f4c9a726d089bfb07af58c
Dan Moore <dan@makeitdoathing.com>
2023-11-08 11:17:04 -0500

    correcting gml

--- examples/python/example.py
+++ examples/python/example.py
@@ -2,5 +2,7 @@ s = input("Type your name: ")
 loop_count = int(input("How many times to repeat"))
 i = 0
 while i < loop_count:
-    print("Hello, " + s)
+    print(f"Hello, {s} {loop_count}")
     i = i + 1
+    
+    
